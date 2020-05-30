f0e852d3b3f37c143cd89bd7def023b25a26bd30
Edward ZEH <e.zeh@gmx.ch>
2020-05-30 15:35:45 +0200

    initial working version

---
+++ ExplorerPathManager/ExplorerPathManager/MainWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using ExplorerPathManager.Annotations;
+
+namespace ExplorerPathManager
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public MainWindowViewModel()
+        {
+            Items.Add("C:\\software_downloads\\Since_2019_Nov");
+            Items.Add("C:\\");
+
+            OpenInFileExplorerCommand = new DelegateCommand((o) =>
+            {
+                string path = o as string;
+                if (path != null)
+                {
+                    WindowsExplorerChangeLocation.Execute(path);
+                }
+            });
+        }
+
+        public ObservableCollection<string> Items { get; set; } = new ObservableCollection<string>();
+
+        public DelegateCommand OpenInFileExplorerCommand { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ ExplorerPathManager/ExplorerPathManager/WindowsExplorerChangeLocation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExplorerPathManager
+{
+    public class WindowsExplorerChangeLocation
+    {
+        public static void Execute(string path)
+        {
+            string dir = Directory.GetCurrentDirectory();
+
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = @"powershell.exe";
+            startInfo.Arguments = $"{dir}\\WindowsExplorerChangeLocation.ps1 -path {path}";
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardError = true;
+            startInfo.UseShellExecute = false;
+            startInfo.CreateNoWindow = true;
+            Process process = new Process();
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+    }
+}
