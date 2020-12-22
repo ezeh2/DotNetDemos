3c1036ad6bfdb01af0def38d5bc8a97c157658c4
Edward ZEH <e.zeh@gmx.ch>
2020-12-22 14:28:36 +0100

    added: Scheduler_Test.Test2()

--- SchedulerConsoleApp/SchedulerConsoleApp_Test/Scheduler_Test.cs
+++ SchedulerConsoleApp/SchedulerConsoleApp_Test/Scheduler_Test.cs
@@ -8,11 +8,25 @@ namespace SchedulerConsoleApp.UnitTests
     class Scheduler_Test
     {
         [Test]
+        [Timeout(20000)]
         public void Test1()
         {
             List<OwnTask> ownTasks = new List<OwnTask>();
             ownTasks.Add(new OwnTask("t1", 5, true));
             ownTasks.Add(new OwnTask("t2", 15, false));
+            ownTasks.Add(new OwnTask("t3", 10, false));
+
+            Scheduler scheduler = new Scheduler();
+            scheduler.ExecuteInParallel(ownTasks);
+        }
+
+        [Test]
+        [Timeout(20000)]
+        public void Test2()
+        {
+            List<OwnTask> ownTasks = new List<OwnTask>();
+            ownTasks.Add(new OwnTask("t1", 5, false));
+            ownTasks.Add(new OwnTask("t2", 15, true));
             ownTasks.Add(new OwnTask("t3", 10, false));
 
             Scheduler scheduler = new Scheduler();
